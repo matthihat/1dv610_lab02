1adc9b40b9c80164365aad225e904568d0893131
Mattias Törnqvist <mtoxr09@student.lnu.se>
2022-10-03 21:45:24 +0200

    lagt till userservice

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AddUserComponent } from './add-user/add-user.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from '../models/user/User.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private users: User[] = [];
+  private userSource = new Subject<User>();
+  public userAdded$ = this.userSource.asObservable();
+
+  constructor() { }
+
+  add(user: User) {
+    //TODO lägga till user i arrayen??
+    this.userSource.next(user);
+  }
+}
